8ba76b51f6e4305d90e667421d19a91c8a25a9e0
steven schkolne <steven@schkolne.com>
2022-11-17 17:18:08 -0800

    set up a single phantom

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import loadable from '@loadable/component';
 import { render } from 'react-dom';
 import 'normalize.css';
 // eslint-disable-next-line import/no-extraneous-dependencies
@@ -10,11 +11,28 @@ import { AuthContext } from './context/auth';
 
 Devign.init();
 
+const phantomBase = `./phantoms/`;
+
+const Ghostbox = ({children}) => {
+	const params = new URLSearchParams(window.location.search);
+	const phantom = params.get('phantom');
+
+	if (phantom) {
+		// TODO: figure out a way to read devign.json, if the user sets
+		//       phantom_dir then we want to use that instead
+    const ComponentObj = loadable(() => import(`${phantomBase}${phantom}.js`));
+    return <ComponentObj/>;
+	}
+	return  children;
+};
+
 render(
   <React.StrictMode>
     <AuthContext.Provider value={{ auth }}>
-      <GlobalStyles />
+      <GlobalStyles/>
+      <Ghostbox>
       <App />
+      </Ghostbox>
     </AuthContext.Provider>
   </React.StrictMode>,
   document.getElementById('root')
---
+++ src/phantoms/phantom.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Phantom = () => {
+    const foo = 12;
+
+	return (
+		<div>{foo}</div>
+	);
+};
+export default Phantom;
