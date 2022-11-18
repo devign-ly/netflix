41492f38b6777b8d54a4e50b911a18924062b0de
steven schkolne <steven@schkolne.com>
2022-11-17 18:16:59 -0800

    added something to my phantom! turned off prettier as it complains about how the styled component got added....

--- src/phantoms/phantom.jsx
+++ src/phantoms/phantom.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
+import styled from "styled-components";
+const MyStyle = styled.div`
+  border: 1px solid blue;
+  color: white;
+`;
 
 const Phantom = () => {
-    const foo = 12;
-
-	return (
-		<div>{foo}</div>
-	);
+  const foo = 12;
+  return <MyStyle>{foo}</MyStyle>;
 };
+
 export default Phantom;
