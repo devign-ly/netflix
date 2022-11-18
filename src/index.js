c5aa88f4f375624a4bfbbbc00c80b2c2856ee1c9
steven schkolne <steven@schkolne.com>
2022-11-18 13:18:15 -0800

    updated a comment

--- src/index.js
+++ src/index.js
@@ -19,14 +19,16 @@ const Ghostbox = ({children}) => {
 
 	if (phantom) {
 		// TODO: figure out a way to read devign.json, if the user sets
-		//       phantom_dir then we want to use that instead
+		//       phantom_dir then we want to use that instead (worst case we can pass it in as url param)
     // XXX: change "jsx" to "js" and watch it break the project, because tests can no longer
-    //      resolve ""../../components"  ... yup, seriously. even if no one uses ComponetObj!
+    //      resolve "../../components"  ... yup, seriously. even if no one uses ComponetObj!
+    // XXX: this also triggers the weird "../../components" error https://www.notion.so/devignly/trying-to-get-Ghostbox-to-pass-a-function-36df73fef60546e0b21ad2291a1cabe3#97fcbf32a53f4430b97e7a55b5b11372
     const ComponentObj = loadable(() => import(`${phantomBase}${phantom}.jsx`));
     return <ComponentObj/>;
 	}
 	return  children;
 };
+
 
 render(
   <React.StrictMode>
