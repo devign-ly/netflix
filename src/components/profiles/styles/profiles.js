95cfe8cf992a76614d2755445aa9f72083dd7347
steven schkolne <steven@schkolne.com>
2023-02-09 14:20:17 -0800

    renamed .devign -> .mightymeld

--- src/components/profiles/styles/profiles.js
+++ src/components/profiles/styles/profiles.js
@@ -11,7 +11,7 @@ export const Container = styled.div`
 
 export const Title = styled.h1`
   width: 100%;
-  color: #fff;
+  color: #d1f8ff;
   font-family: var(--font-title);
   font-size: var(--text-3xl);
   text-align: center;
