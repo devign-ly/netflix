c5c212859c2c1c874443d0f30cb592a751e5d760
MightyMeld <info@mightymeld.com>
2023-03-14 17:35:24 +0000

    'mightymeld commit: 2023/3/14 @ 17:35:24'

--- src/components/profiles/styles/profiles.js
+++ src/components/profiles/styles/profiles.js
@@ -11,7 +11,7 @@ export const Container = styled.div`
 
 export const Title = styled.h1`
   width: 100%;
-  color: #ffffff;
+  color: #ebf99a;
   font-family: var(--font-title);
   font-size: var(--text-3xl);
   text-align: center;
