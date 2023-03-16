656d1cfd51b462aa5ac977fbe45282bc48cb230a
MightyMeld <info@mightymeld.com>
2023-03-16 23:06:05 +0000

    'mightymeld commit: 2023/3/16 @ 23:6:5'

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Container, Row, Column, Link, Title, Break } from './styles/footer';
+import Button from '../button';
 
 export default function Footer({ children }) {
   return <Container>{children}</Container>;
@@ -24,3 +25,23 @@ export function FooterTitle({ children }) {
 export function FooterBreak() {
   return <Break />;
 }
+export const LiveChat = () => {
+  return (
+    <Help
+      style={{
+        display: 'flex',
+        flexDirection: 'row',
+        gap: '10px',
+        justifyContent: 'space-between',
+      }}
+    >
+      <img src="https://randomuser.me/api/portraits/men/28.jpg" />
+
+      <ChatText>
+        <LiveChat>I'm here to help</LiveChat>
+
+        <Button>Chat Now</Button>
+      </ChatText>
+    </Help>
+  );
+};
