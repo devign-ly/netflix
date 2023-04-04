b033847d4c371e2f85bae41a48eb4bc16cd7fde3
Daniel Worthington <daniel@halffullheart.com>
2023-04-04 09:07:50 -0700

    'mightymeld commit: 2023/4/4 @ 9:7:50'

---
+++ src/previews/preview__NewPreview6.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import styled from 'styled-components';
+const MyStyle = styled.h1`
+  color: #b6b1eb;
+  padding: 1em;
+  margin: 0;
+  font-family: sans-serif;
+`;
+
+const Preview = () => {
+  return <MyStyle>Hello World</MyStyle>;
+};
+export default Preview;
