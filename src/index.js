0608d8e8ea46c1171a2592d36c7cfbc082d9ff62
Daniel Worthington <daniel@halffullheart.com>
2023-04-14 13:35:29 -0700

    Remove explicit devigner and ghostbox inclusion

--- src/index.js
+++ src/index.js
@@ -1,14 +1,10 @@
 import React from 'react';
 import { render } from 'react-dom';
 import 'normalize.css';
-// eslint-disable-next-line import/no-extraneous-dependencies
-import * as MightyMeld from 'mightymeld';
 import { GlobalStyles } from './global-styles';
 import { App } from './app';
 import { auth } from './lib/auth';
 import { AuthContext } from './context/auth';
-
-MightyMeld.init();
 
 render(
   <React.StrictMode>
