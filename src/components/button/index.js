182eb713b155a066245b8d16cb583f73fb1404ac
Daniel Worthington <daniel@halffullheart.com>
2024-06-26 16:45:27 -0600

    This is a test commit

--- src/components/button/index.js
+++ src/components/button/index.js
@@ -2,6 +2,9 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import * as Styles from './styles/button';
 
+
+// test
+
 export default function Button({ element, size, type, disabled, to, children }) {
   return (
     <Styles.Button type={type} disabled={disabled} to={to} className={size} as={element === 'link' ? Link : null}>
